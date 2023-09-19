4d25c230945c4c5026ae63c55e024a77f448d347
Fatih İdris Geçgin <fatih-gecgin@hotmail.com>
2023-09-19 17:16:46 +0300

    Create Students with db, form and creating view page
    Create Students with db, form and creating view page

--- Controllers/OgrenciController.cs
+++ Controllers/OgrenciController.cs
@@ -1,12 +1,26 @@
+using EfCoreKursApp.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EfCoreKursApp.Controllers
 {
-    public class OgrenciControllers : Controller
+    public class OgrenciController : Controller
     {
+        private readonly DataContext _context;
+        public OgrenciController(DataContext context)
+        {
+            _context = context;
+        }
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Ogrenci model)
+        {
+            _context.Ogrenciler.Add(model);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index","Home");
         }
     }
 }
