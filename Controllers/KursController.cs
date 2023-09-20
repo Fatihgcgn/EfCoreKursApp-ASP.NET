7db27a2711b5fccee0f3f0e53d877c4bcbac067a
Fatih İdris Geçgin <fatih-gecgin@hotmail.com>
2023-09-20 16:29:31 +0300

    Kurs Create ve Index sayfaları yapıldı
    Kurs Create ve Index sayfaları yapıldı

---
+++ Controllers/KursController.cs
@@ -0,0 +1,33 @@
+using EfCoreKursApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfCoreKursApp.Controllers
+{
+    public class KursController : Controller
+    {
+        private readonly DataContext _context;
+        public KursController(DataContext context)
+        {
+            _context = context;
+        }
+
+         public async Task<IActionResult> Index()
+        {
+            var kurslar = await _context.Kurslar.ToListAsync();
+            return View(kurslar);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Kurs model)
+        {
+            _context.Kurslar.Add(model);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
