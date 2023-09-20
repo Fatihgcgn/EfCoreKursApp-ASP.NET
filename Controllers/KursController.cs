7705a267804730064ceffa2e64a03558aa463476
Fatih İdris Geçgin <fatih-gecgin@hotmail.com>
2023-09-20 17:46:40 +0300

    Kurs Edit
    Kurs Edit

--- Controllers/KursController.cs
+++ Controllers/KursController.cs
@@ -23,11 +23,63 @@ namespace EfCoreKursApp.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(Kurs model)
         {
             _context.Kurslar.Add(model);
             await _context.SaveChangesAsync();
             return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var kurs = await _context.Kurslar.FindAsync(id);
+            // var ogr = await _context.Ogrenciler.FirstOrDefaultAsync(o =>o.OgrenciId == id);
+
+            if (kurs == null)
+            {
+                return NotFound();
+            }
+
+            return View(kurs);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id,Kurs model)
+        {
+            if (id != model.KursId)
+            {
+                return NotFound();
+            }
+
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(model);
+                    await _context.SaveChangesAsync();
+                }
+                catch(DbUpdateException)
+                {
+                    if(!_context.Ogrenciler.Any(o => o.OgrenciId == model.KursId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
         }
     }
 }
